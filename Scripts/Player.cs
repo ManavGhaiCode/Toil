1e9c4740c80421bdab156d6ad8f02b60d06de6ef
ManavGhaiCode <manavghai12october@gmail.com>
2024-05-23 08:49:49 +0530

    add Dashing for the player

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float Speed = 5f;
+    public float DashCooldown = 10f;
+    public float DashInvincibilityTime = 0.25f;
 
     private Rigidbody2D rb;
     private Vector2 MoveInput;
@@ -11,24 +13,59 @@ public class Player : MonoBehaviour {
     [SerializeField] private int Health;
     [SerializeField] private Weapon Gun;
 
-    void Start() {
+    private bool canDash = true;
+    private bool isDashing = false;
+    private bool canTakeDamage = true;
+
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
+    private void Update() {
         MoveInput.x = Input.GetAxisRaw("Horizontal");
         MoveInput.y = Input.GetAxisRaw("Vertical");
 
         if (Input.GetKey(KeyCode.Mouse0)) {
             Gun.Attack();
         }
+
+        isDashing = Input.GetKey(KeyCode.Space);
     }
 
-    void FixedUpdate() {
+    private void FixedUpdate() {
         rb.MovePosition(rb.position + (MoveInput.normalized * Speed * Time.fixedDeltaTime));
+
+        if (isDashing) {
+            if (!canDash) return;
+
+            Dash();
+            isDashing = false;
+        }
+    }
+
+    private void Dash() {
+        Speed *= 2.5f;
+        canTakeDamage = false;
+        canDash = false;
+
+        StartCoroutine(ResentCanTakeDamage());
+        StartCoroutine(ResentCanDash());
+    }
+
+    private IEnumerator ResentCanTakeDamage() {
+        yield return new WaitForSeconds (DashInvincibilityTime);
+        canTakeDamage = true;
+        Speed /= 2.5f;
+    }
+
+    private IEnumerator ResentCanDash() {
+        yield return new WaitForSeconds (DashCooldown);
+        canDash = true;
     }
 
     public void TakeDamage(int Damage) {
+        if (!canTakeDamage) return;
+
         Health -= Damage;
 
         if (Health <= 0) {
